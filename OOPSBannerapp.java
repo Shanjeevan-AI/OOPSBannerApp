e6a2f92dcd468189aaab192baf2d6d1c4854a499
Shanjeevan-AI <sd5534@srmist.edu.in>
2026-03-07 16:16:36 +0530

    UC5 OOPSBannerapp Using inline array

--- OOPSBannerapp.java
+++ OOPSBannerapp.java
@@ -1,13 +1,16 @@
 public class OOPSBannerapp {
     public static void main(String[] args){
-          System.out.println(String.join(" ","   ***   "," ","   ***   ","*****    "," ","   *****  "));
-		  System.out.println(String.join(" "," **   ** "," "," **   ** ","**   **  "," "," **       "));
-	      System.out.println(String.join(" ","**     **"," ","**     **","**    ** "," ","**        "));
-		  System.out.println(String.join(" ","**     **"," ","**     **","**   **  "," "," **       "));
-		  System.out.println(String.join(" ","**     **"," ","**     **","*****    "," ","   ***    "));
-		  System.out.println(String.join(" ","**     **"," ","**     **","**       "," ","      **  "));
-		  System.out.println(String.join(" ","**     **"," ","**     **","**       "," ","       ** "));
-		  System.out.println(String.join(" "," **   ** "," "," **   ** ","**       "," ","      **  "));
-		  System.out.println(String.join(" ","   ***   "," ","   ***   ","**       "," "," *****    "));
+          String[] lines = { 
+		   String.join(" ","   ***   "," ","   ***   ","*****    "," ","   *****  "),
+		   String.join(" "," **   ** "," "," **   ** ","**   **  "," "," **       "),
+	       String.join(" ","**     **"," ","**     **","**    ** "," ","**        "),
+		   String.join(" ","**     **"," ","**     **","**   **  "," "," **       "),
+		   String.join(" ","**     **"," ","**     **","*****    "," ","   ***    "),
+		   String.join(" ","**     **"," ","**     **","**       "," ","      **  "),
+		   String.join(" ","**     **"," ","**     **","**       "," ","       ** "),
+		   String.join(" "," **   ** "," "," **   ** ","**       "," ","      **  "),
+		   String.join(" ","   ***   "," ","   ***   ","**       "," "," *****    ")};
+		for (String line : lines){ 
+		System.out.println(line);}
 	}
 }
